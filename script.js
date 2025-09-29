1c5e1f1f0b8f04076dd9c091f760c569bb7989cd
rominamartin2128 <108691359+rominamartin2128@users.noreply.github.com>
2025-09-29 02:22:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const contenedor = document.getElementById("catalogo");
+const contenedor = document.getElementById("catalogoContainer");
 
 // Tomo los productos del JSON
 fetch("productos.json")
@@ -34,5 +34,4 @@ fetch("productos.json")
       contenedor.appendChild(card);
     });
   });
-
 
